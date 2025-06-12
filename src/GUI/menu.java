c91def58c915831fee281b443c04561b0a3fbede
Jimmi calvo <jimmyldshoyos@gmail.com>
2025-06-11 22:54:24 -0500

    adelanto modo historia

--- src/GUI/menu.java
+++ src/GUI/menu.java
@@ -4,6 +4,8 @@
  */
 package GUI;
 
+import GUI.historia.*;
+
 /**
  *
  * @author david
@@ -86,7 +88,10 @@ public class menu extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+      menuHistoria menuH = new menuHistoria();
+        dispose();
+        
+        menuH.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
